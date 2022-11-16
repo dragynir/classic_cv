77bf7df76c1fbb678d040efd6f87e103e3edf975
dragynir <d.korostelev@g.nsu.ru>
2022-11-16 20:58:57 +0700

    lab 7

--- src/app/filters.py
+++ src/app/filters.py
@@ -4,11 +4,11 @@ import numpy as np
 from image_utils import IMAGES, plot_two_images
 
 
-def apply_filter(image, filter=None, callable=None, args=None):
+def apply_filter(image, kernel=None, callable=None, args=None):
     assert (filter is not None or callable is not None)
 
-    if filter is not None:
-        result = cv2.filter2D(src=image, ddepth=-1, kernel=filter)
+    if kernel is not None:
+        result = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
     elif callable is not None:
         result = callable(image, **args)
     else:
@@ -26,16 +26,20 @@ if __name__ == '__main__':
         'laplacian': (cv2.Laplacian, {'ddepth': -1, 'ksize': 3}),
     }
 
-    filters = {
-        'blur': np.ones((5, 5)),
-        'sobel_gy': np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]),
-        'soble_gx': np.array([[1, 2, 1], [0, 0, 0], [-1, -2, 1]]),
+    kernels = {
+        'blur': np.ones((3, 3)),
+        'sobel_gx': np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]),
+        'soble_gy': np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]),
     }
 
     image = IMAGES["lines"]
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    for name, config in operations.items():
-        op, args = config
-        result = apply_filter(image, callable=op, args=args)
-        plot_two_images(image, result["out_image"], title=name)
+    # for name, config in operations.items():
+    #     op, args = config
+    #     result = apply_filter(image, callable=op, args=args)
+    #     plot_two_images(image, result["out_image"], title=name)
+
+    for name, kernel in kernels.items():
+        result = apply_filter(gray_image, kernel=kernel)
+        plot_two_images(gray_image, result["out_image"], title=name)
