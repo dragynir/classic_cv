96ca9339b953e8bc29fefdbc59218ae38a6ad50e
dragynir <d.korostelev@g.nsu.ru>
2022-11-17 15:27:19 +0700

    lab 9

--- src/app/features.py
+++ src/app/features.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
-
+import imutils
 from image_utils import IMAGES, plot_two_images
 
 
-def find_features(image: np.ndarray, threshold=0.01):
+def find_harris_features(image: np.ndarray, threshold=0.01):
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -21,12 +21,39 @@ def find_features(image: np.ndarray, threshold=0.01):
     }
 
 
+def find_tomas_features(image: np.ndarray, top_n=20):
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    features = cv2.goodFeaturesToTrack(gray, top_n, 0.01, 10)
+    features = np.int0(features)
+
+    image_out = image.copy()
+
+    # Iterate over the corners and draw a circle at that location
+    for i in features:
+        x, y = i.ravel()
+        cv2.circle(image_out, (x, y), 5, (0, 0, 255), -1)
+
+    return {
+        'image_out': image_out,
+        'features': features,
+    }
+
+# def test_rotation(image, target_features, angles=(0, 360)):
+#
+#     rot = imutils.rotate(image, angle=45)
+
+
 if __name__ == '__main__':
     # like repeatability
     # metrics to estimate: https://sbme-tutorials.github.io/2018/cv/notes/9_week9.html
     # надо построить графиг для повторяемости для разных углов и т д
     image = IMAGES["lines"]
 
-    result = find_features(image, threshold=0.1)
-
+    # result = find_features(image, threshold=0.1)
+    result = find_tomas_features(image)
     plot_two_images(image, result["image_out"])
+
+
+
